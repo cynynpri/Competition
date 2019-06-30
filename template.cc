f34c78e4aeb247db509daf13a9d1c696ee2662e5
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2019-06-30 23:41:25 +0900

    improved comment space.

--- template.cc
+++ template.cc
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>// using gnu c++ compiler.
+#include <bits/stdc++.h>  // using gnu c++ compiler.
 //#include <iostream>
 //#include <cstdio>
 //#include <vector>
@@ -6,19 +6,19 @@
 //#include <string>
 //#include <map>
 
-using namespace std;// if U using this code in a project, U are fired.
-using ll = long long;// using `g++ hoge.cc -o msain.out -Wall -Wextra -std=c++11`
-// typedef long long ll;// needless -std settings.
+using namespace std;  // if U using this code in a project, U are fired.
+using ll = long long;  // using `g++ hoge.cc -o msain.out -Wall -Wextra -std=c++11`
+// typedef long long ll;  // needless -std settings.
 
-// FYI
-//url : https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+// @FYI
+// @url : https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
 
 #define rep(i, n) for(int i = 0, i##_len = (n); i < i##_len; ++i)
 #define _rep(i, n) for(int i = (n) - 1; i > 0; --i)
 #define all(vec) (vec).begin(), (vec).end()
 
-// special functions
-// `ll comb` is `ll modpow` required.
+// @special functions
+// @comment : `ll comb` is `ll modpow` required.
 
 // int gcd(int r, int l){return l?gcd(l, r%l):r;}
 // ll modinv(ll a, ll m){ ll b=m, u=1, v=0; while(b){ll t = a/b; a-=t*b; swap(a,b); u-=t*v; swap(u,v);} u%=m; if(u<0)u+=m; return u;}
@@ -26,12 +26,12 @@ using ll = long long;// using `g++ hoge.cc -o msain.out -Wall -Wextra -std=c++11
 // ll modpow(ll x, ll n, ll mod){ ll res = 1; while(n > 0){ if(n & 1){ res *= x; res %= mod;} x *= x; x %= mod; n >>= 1;} return res;}
 // ll comb(ll a, ll b, ll p){ if (b > a-b) return comb(a, a-b, p); ll c=1, d=1; for(ll i = 0; i < b; i++){c*=(a-i);d*=(b-i);c%=p;d%=p;} return c * modpow(d, p-2, p) % p;}
 
-// int _modpow(int x, int n, int mod); // TODO : comment in `int _modpow`.
-// int _comb(int x, int b, int p); // TODO : comment in `int _comb` and `int _modpow`.
+// int _modpow(int x, int n, int mod);  // TODO : comment in `int _modpow`.
+// int _comb(int x, int b, int p);  // TODO : comment in `int _comb` and `int _modpow`.
 
 int main()
 {
-  //write here.
+  // write here.
 
   return 0;
 }
