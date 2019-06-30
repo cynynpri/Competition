9546a7a6bac7213f06b9961b880292e854e360dd
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2019-06-30 22:58:27 +0900

    add my template.

---
+++ template.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, n) for(int i = 0, i##_len = (n); i < i##_len; ++i)
+#define all(vec) (vec).begin(), (vec).end()
+// int gcd(int r, int l){return l?gcd(l, r%l):r;}
+// int qp(int a,ll b){int ans=1;do{if(b&1)ans=1ll*ans*a%mo;a=1ll*a*a%mo;}while(b>>=1);return ans;}
+// int qp(int a,ll b,int mo){int ans=1;do{if(b&1)ans=1ll*ans*a%mo;a=1ll*a*a%mo;}while(b>>=1);return ans;}
+using ll = long long;
+//typedef long long ll;
+
+int main()
+{
+  //
+  return 0;
+}
