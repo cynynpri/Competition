227ed8e65f4b9527b1b7092e44233a33796f9f46
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2019-06-30 23:56:37 +0900

    improved int -> signed.

--- template.cc
+++ template.cc
@@ -9,27 +9,28 @@
 using namespace std;  // if U using this code in a project, U are fired.
 using ll = long long;  // using `g++ hoge.cc -o msain.out -Wall -Wextra -std=c++11`
 // typedef long long ll;  // needless -std settings.
+// typedef long long int;  // u got a greatest power. rarely using.
 
 // @FYI
 // @url : https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
 
-#define rep(i, n) for(int i = 0, i##_len = (n); i < i##_len; ++i)
-#define _rep(i, n) for(int i = (n) - 1; i > 0; --i)
+#define rep(i, n) for(signed i = 0, i##_len = (n); i < i##_len; ++i)
+#define _rep(i, n) for(signed i = (n) - 1; i > 0; --i)
 #define all(vec) (vec).begin(), (vec).end()
 
 // @special functions
 // @comment : `ll comb` is `ll modpow` required.
 
-// int gcd(int r, int l){return l?gcd(l, r%l):r;}
-// ll modinv(ll a, ll m){ ll b=m, u=1, v=0; while(b){ll t = a/b; a-=t*b; swap(a,b); u-=t*v; swap(u,v);} u%=m; if(u<0)u+=m; return u;}
+// signed gcd(signed r, signed l){return l?gcd(l, r%l):r;}
+// ll modinv(ll a, ll m){ ll b=m, u=1ll, v=0ll; while(b){ll t = a/b; a-=t*b; swap(a,b); u-=t*v; swap(u,v);} u%=m; if(u<0)u+=m; return u;}
 // ll minusmod(ll val, ll m){ ll res = val%m; if(res<0)res+=m; return res;}  // minus value mod of long long.
-// ll modpow(ll x, ll n, ll mod){ ll res = 1; while(n > 0){ if(n & 1){ res *= x; res %= mod;} x *= x; x %= mod; n >>= 1;} return res;}
-// ll comb(ll a, ll b, ll p){ if (b > a-b) return comb(a, a-b, p); ll c=1, d=1; for(ll i = 0; i < b; i++){c*=(a-i);d*=(b-i);c%=p;d%=p;} return c * modpow(d, p-2, p) % p;}
+// ll modpow(ll x, ll n, ll mod){ll res = 1ll; while(n > 0ll){ if(n & 1ll){ res *= x; res %= mod;} x *= x; x %= mod; n >>= 1ll;} return res;}
+// ll comb(ll a, ll b, ll p){ if (b > a-b) return comb(a, a-b, p); ll c=1ll, d=1ll; for(ll i=0ll; i<b; ++i){c*=(a-i); d*=(b-i); c%=p; d%=p;} return c * modpow(d, p-2ll, p)%p;}
 
-// int _modpow(int x, int n, int mod);  // TODO : comment in `int _modpow`.
-// int _comb(int x, int b, int p);  // TODO : comment in `int _comb` and `int _modpow`.
+// signed _modpow(signed x, signed n, signed mod);  // TODO : comment in `signed _modpow`.
+// signed _comb(signed x, signed b, signed p);  // TODO : comment in `signed _comb` and `signed _modpow`.
 
-int main()
+signed main()
 {
   // write here.
 
@@ -37,8 +38,8 @@ int main()
 }
 
 /*
-int _modpow(int x, int n, int mod){
-  int res = 1;
+signed _modpow(signed x, signed n, signed mod){
+  signed res = 1;
   while(n > 0){
     if(n & 1){  // nが奇数のとき
       res *= x;
@@ -53,10 +54,10 @@ int _modpow(int x, int n, int mod){
 */
 
 /*
-int _comb(int a, int b, int p){
+signed _comb(signed a, signed b, signed p){
   if(b > a - b) return _comb(a, a-b, p);
-  int c=1, d=1;
-  for(int i = 0; i < b; i++){
+  signed c=1, d=1;
+  for(signed i = 0; i < b; i++){
     c *= (a-i);
     d *= (b-i);
     c %= p;
