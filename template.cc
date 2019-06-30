917a09849bdf041299811c0a1e0a3e2e92868cca
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2019-06-30 23:44:26 +0900

    improved space.

--- template.cc
+++ template.cc
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>  // using gnu c++ compiler.
-//#include <iostream>
-//#include <cstdio>
-//#include <vector>
-//#include <algorithm>
-//#include <string>
-//#include <map>
+// #include <iostream>
+// #include <cstdio>
+// #include <vector>
+// #include <algorithm>
+// #include <string>
+// #include <map>
 
 using namespace std;  // if U using this code in a project, U are fired.
 using ll = long long;  // using `g++ hoge.cc -o msain.out -Wall -Wextra -std=c++11`
@@ -22,7 +22,7 @@ using ll = long long;  // using `g++ hoge.cc -o msain.out -Wall -Wextra -std=c++
 
 // int gcd(int r, int l){return l?gcd(l, r%l):r;}
 // ll modinv(ll a, ll m){ ll b=m, u=1, v=0; while(b){ll t = a/b; a-=t*b; swap(a,b); u-=t*v; swap(u,v);} u%=m; if(u<0)u+=m; return u;}
-// ll minusmod(ll val, ll m){ ll res = val%m; if(res<0)res+=m; return res;} // minus value mod of long long.
+// ll minusmod(ll val, ll m){ ll res = val%m; if(res<0)res+=m; return res;}  // minus value mod of long long.
 // ll modpow(ll x, ll n, ll mod){ ll res = 1; while(n > 0){ if(n & 1){ res *= x; res %= mod;} x *= x; x %= mod; n >>= 1;} return res;}
 // ll comb(ll a, ll b, ll p){ if (b > a-b) return comb(a, a-b, p); ll c=1, d=1; for(ll i = 0; i < b; i++){c*=(a-i);d*=(b-i);c%=p;d%=p;} return c * modpow(d, p-2, p) % p;}
 
