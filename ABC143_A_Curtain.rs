756713240633487c64208274d28f7adcfcf2c567
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2019-10-28 10:20:26 +0900

    improved compile error.

--- ABC143_A_Curtain.rs
+++ ABC143_A_Curtain.rs
@@ -1,13 +1,13 @@
 use std::io;
 
-fn getstdin_i32() :Vec<i32> {
+fn getstdin_i32() -> Vec<i32> {
     let mut iobuffer: String = String::new();
     io::stdin().read_line(&mut iobuffer).ok();
     iobuffer.pop();  // remove '\n';
 
     let buf_strvec: Vec<&str> = iobuffer.split(' ').collect();
     let mut rtn_vec_i32: Vec<i32> = Vec::new();
-    for s: &str in buf_strvec{
+    for s in buf_strvec{
         rtn_vec_i32.push(s.parse().unwrap());
     }
 
