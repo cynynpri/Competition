5c153ba144f94052fc9d4d504168903a6db630b7
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2019-10-28 10:20:00 +0900

    improved my rust practice code.

--- ABC143_A_Curtain.rs
+++ ABC143_A_Curtain.rs
@@ -1,13 +1,23 @@
 use std::io;
 
+fn getstdin_i32() :Vec<i32> {
+    let mut iobuffer: String = String::new();
+    io::stdin().read_line(&mut iobuffer).ok();
+    iobuffer.pop();  // remove '\n';
+
+    let buf_strvec: Vec<&str> = iobuffer.split(' ').collect();
+    let mut rtn_vec_i32: Vec<i32> = Vec::new();
+    for s: &str in buf_strvec{
+        rtn_vec_i32.push(s.parse().unwrap());
+    }
+
+    return rtn_vec_i32;
+}
+
 fn main() {
-    let mut raw_s: String = String::new();
-    io::stdin().read_line(&mut raw_s).ok();
-    raw_s.pop();
-    
-    let s_vec: Vec<&str> = raw_s.split(' ').collect();
-    let A: i32 = s_vec[0].parse().unwrap();
-    let B: i32 = s_vec[1].parse().unwrap();
+    let mut in_vec: Vec<i32> = getstdin_i32();
+    let A: i32 = in_vec[0];
+    let B: i32 = in_vec[1];
     let mut ans: i32 = A - B*2;
     if ans < 0 {
         ans = 0;
